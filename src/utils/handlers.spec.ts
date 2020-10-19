ccedf36ff9d320805e0cf1c7df5907ccab75fe28
Jack Williams <jpwilliamsphotography@gmail.com>
2020-10-19 18:18:51 +0100

    test(utils/handlers): add test stubs

---
+++ src/utils/handlers.spec.ts
@@ -0,0 +1,10 @@
+// local
+// import { wrapHandler } from "./handlers";
+
+describe("wrapHandler", () => {
+  test.todo("Returns non-function as static data");
+  test.todo("Returns serialised error on sync throw");
+  test.todo("Returns serialised error on async throw");
+  test.todo("Returns data from sync function");
+  test.todo("Returns data from async function");
+});
