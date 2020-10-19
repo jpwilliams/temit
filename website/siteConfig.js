6be07ec841f8ffe1ea9d94d57d4c9e5d7ab8219a
Jack Williams <jpwilliamsphotography@gmail.com>
2020-10-19 18:16:03 +0100

    docs(website/siteConfig): add external links to other sites

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,9 +38,23 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    { doc: "introduction", label: "Docs" },
+    { doc: "introduction", label: "Getting Started" },
     { doc: "api/temit", label: "API" },
-    { blog: true, label: "Blog" },
+    {
+      href: "https://github.com/jpwilliams/temit",
+      label: "Github",
+      external: true,
+    },
+    {
+      href: "https://www.npmjs.com/package/temit",
+      label: "npm",
+      external: true,
+    },
+    {
+      href: "https://discord.gg/PWs8FG4",
+      label: "Discord",
+      external: true,
+    },
   ],
 
   // If you have users set above, you add it here:
